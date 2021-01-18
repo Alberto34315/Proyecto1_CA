8070d4996d5408f4316ab30f92f58e309e126960
Jorge SB <jorge.trabajo42@gmail.com>
2021-01-18 19:40:00 +0100

    DAO y CONTROLLER

---
+++ src/main/java/controller/cuentaController.java
@@ -0,0 +1,72 @@
+package controller;
+
+import DAO.cuentaDAO;
+import java.util.List;
+import model.Cuenta;
+
+public class cuentaController {
+    
+    private static cuentaController instancia = null;
+    
+    public static cuentaController getInstance() {
+        instancia = new cuentaController();
+        return instancia;
+    }
+
+    public List<Cuenta> getAllCuentas() {
+        cuentaDAO aDAO = new cuentaDAO();
+        return aDAO.getAll();
+    }
+
+    public Cuenta getCuentaById(int codigoCuenta) {
+        cuentaDAO aDAO = new cuentaDAO();
+        return aDAO.getByID(codigoCuenta);
+    }
+
+    public boolean insertCuenta(Cuenta a) {
+        boolean result = false;
+        if (a != null) {
+            cuentaDAO aDAO = new cuentaDAO();
+            aDAO.insert(a);
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean editCuenta(Cuenta a) {
+        boolean result = false;
+        if (a != null) {
+            cuentaDAO aDAO = new cuentaDAO();
+            aDAO.edit(a);
+            result = true;
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    public boolean removeCuenta(Cuenta a) {
+        boolean result = false;
+        if (a != null) {
+            cuentaDAO aDAO = new cuentaDAO();
+            aDAO.remove(a);
+        } else {
+            result = false;
+        }
+        return result;
+    }
+    
+    /*
+    public List<Disco> getRepertorio(int ID) {
+        ArtistaDAO aDAO = new ArtistaDAO();
+        return aDAO.getListRepertorio(ID);
+    }
+    */
+
+    public boolean searchByCuentaID(int codigoCuenta) {
+        cuentaDAO aDAO = new cuentaDAO();
+        return aDAO.searchByID(codigoCuenta);
+    }
+}
